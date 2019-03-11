ca932e708b0217590d28f5abf936a05643ecacb2
Korbin Griffin <02korbin.griffin@gulllakecs.org>
2019-03-11 12:56:09 -0400

    add variables

--- Baseball.py
+++ Baseball.py
@@ -1,3 +1,14 @@
-# The Hitting Metrics of Hitting
+# The Hitting Metrics of Baseball
 # Korbin Griffin
 
+
+
+for i in range(3):
+    speed_of_pitch = int(input("What is the speed of the pitch?: "))
+    launch_angle = int(input("What is the angle you hit the ball at?: "))
+    speed_of_bat = int(input("What is the speed of the bat?: "))
+    speed_of_outfielder =  8.8
+    overall_speed_of_ball = speed_of_bat - speed_of_pitch
+    while launch_angle >= 0:
+        print "hit in play"
+        
