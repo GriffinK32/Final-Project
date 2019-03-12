d0fa49019d85eb4ef538b01a15089b5dfaaf55b1
Korbin Griffin <02korbin.griffin@gulllakecs.org>
2019-03-12 12:18:48 -0400

    add while_loop

--- Baseball.py
+++ Baseball.py
@@ -1,14 +1,25 @@
 # The Hitting Metrics of Baseball
 # Korbin Griffin
 
-
+bat = x
 
 for i in range(3):
     speed_of_pitch = int(input("What is the speed of the pitch?: "))
     launch_angle = int(input("What is the angle you hit the ball at?: "))
     speed_of_bat = int(input("What is the speed of the bat?: "))
-    speed_of_outfielder =  8.8
+    speed_of_outfielder = 8.8
     overall_speed_of_ball = speed_of_bat - speed_of_pitch
     while launch_angle >= 0:
-        print "hit in play"
+        print("hit in play")
+        if launch_angle >= 5:
+            print ("You're Out!")
+            break
+        elif launch_angle == (6,15):
+            x = print ("You Got a Single!")
+            break
+        else:
+            print("you hit high enough to get more than a single")
+    
+
+
 
